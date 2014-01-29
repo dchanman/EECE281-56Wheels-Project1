4c7ae78503e628fc80c5f4974a4520eda684cd48
sashadordjiev <sashadordjiev@gmail.com>
2014-01-29 00:48:22 -0800

    Wrote description of Derek's Part

--- Controller_Output.asm
+++ Controller_Output.asm
@@ -3,6 +3,12 @@ $NOLIST
 ;	Controller Output
 ; 
 ;	Derek Chan
+;	
+;	Function: Controls everything and calls on everyone in the program, also 
+;	handles any errors ie: door opening / temperature dropping
+;	1) Calls the User Interface Function to get the values of each  
+; 	2) Handles timing/interupts 
+;	3) 
 ;----------------------------------------------------
 CSEG
 
