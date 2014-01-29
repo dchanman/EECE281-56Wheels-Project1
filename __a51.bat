35295aae81450c51ea122845bcb2a9a3e6f5a817
sashadordjiev <sashadordjiev@gmail.com>
2014-01-28 21:56:39 -0800

    Removing Extra .bat file

--- __a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\Derek\Documents\GitHub\EECE281-56Wheels-Project1\"
-"C:\Program Files\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\Derek\Documents\GitHub\EECE281-56Wheels-Project1\main.asm"
-echo Crosside_Action Set_Hex_File C:\Users\Derek\Documents\GitHub\EECE281-56Wheels-Project1\main.HEX
