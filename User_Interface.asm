dc1470200fb1479ce03c80eb019dc86b63f31bc8
sashadordjiev <sashadordjiev@gmail.com>
2014-01-30 18:37:52 -0800

    Outlining Functions
    Outlining initialization function more

--- User_Interface.asm
+++ User_Interface.asm
@@ -8,6 +8,10 @@ $NOLIST
 ;				Called at the beginning of the code, this is the user interface
 ;				function used to get the temperature settings and stores them 
 ;				into registers. 
+						1.1)Welcome_message
+						1.2)Soak_Temperature_Input
+						1.3)Reflow_Temperature_Input
+						1.4)Reflow_Time_Input
 ;
 ;  			  2)Checking for other inputs (Check_Inputs)
 ;				Provides a check to the other user inputs used during the heating
@@ -20,22 +24,59 @@ $NOLIST
 ;	Inputs: 	1) Switches 0-9 for inputing digits
 ;				2) Switch 10 for Pressing Enter
 ;				3) Switch 11 for Pressing Back
+;				4)Switch 12 for switching between Celcius/Kelvin/Farhenheit
 ;
-;	Outputs: 	1)HEX Display (writes the correct value to x+0 and x+1, and calls 	;				hex2bcd and calls display)
+;	Outputs: 	1)HEX Display (writes the correct value to x+0 and x+1, and calls hex2bcd and display
 ;				2) LCD Display depending on status (Derek's portion) displays the
 ;				current state that it is in (Set Up/Heating/Cooldown etc) and
 ;				also displays what you want to enter in the initialization portion
 ;
-;	Memory: 	1) Soak Temp - R7
-;				2) 
+;	Memory: 	1) Soak Temperature - soak_temperature
+;				2) Soak Time - soak_time
+;				3) Reflow Temperature - reflow_temperature
+;				4) Reflow Time        - relow_time
+;					note: time is given is seconds, and temperature is given in degrees C. both take two registers to store
 ;
 
 ;----------------------------------------------------
 CSEG
 
 User_Interface_Init:
+	lcall Settings_Initialization	
+ret
 	
 
-	ret
-	
+
+;Function: Gets the correct parameters for over control from the user
+Settings_Initialization:
+	lcall Welcome_message
+	lcall Soak_temperature_Input
+	lcall Soak_Time_Input
+	lcall Relow_Temperature_Input
+	lcall Reflow_Time_Input
+ret
+
+
+;Function: Displays the welcome message 
+;			("Welcome! Please enter oven parameters")
+Welcome_message:
+ret
+
+;Function: Waits for user to input the soak temperature
+Soak_Temperature_Input:
+ret
+
+;Function: Waits for user to input the soak time
+Soak_Time_Input:
+ret
+
+;Function: Waits for user to input the reflow temperature
+Reflow_Temperature_Input:
+ret
+
+;Function: Waits for user to input the reflow time
+Reflow_Time_Input:
+ret
+
+
 $LIST
