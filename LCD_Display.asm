c8b18c869d7b674820e16d78508aeeb66a90365e
jgmhua <jhua@live.ca>
2014-01-31 15:44:34 -0800

    Making File LCD_Display.asm
    Putting all the functions needed to write to the LCD_Display

---
+++ LCD_Display.asm
@@ -0,0 +1,74 @@
+;Function:  1)To initialize the LCD Display
+;			2)To be able to write text to the LCD
+;
+;			note: functions taken from spi.logger.asm
+;
+;			Functions added:
+;			1)
+;
+
+
+Wait40us:
+	mov R0, #149
+Wait40us_L0: 
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	djnz R0, Wait40us_L0 ; 9 machine cycles-> 9*30ns*149=40us
+    ret
+
+LCD_command:
+	mov	LCD_DATA, A
+	clr	LCD_RS
+	nop
+	nop
+	setb LCD_EN ; Enable pulse should be at least 230 ns
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	clr	LCD_EN
+	ljmp Wait40us
+
+LCD_put:
+	mov	LCD_DATA, A
+	setb LCD_RS
+	nop
+	nop
+	setb LCD_EN ; Enable pulse should be at least 230 ns
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	clr	LCD_EN
+	ljmp Wait40us
+
+LCD_Init:
+    ; Turn LCD on, and wait a bit.
+    setb LCD_ON
+    clr LCD_EN  ; Default state of enable must be zero
+    lcall Wait40us
+    
+    mov LCD_MOD, #0xff ; Use LCD_DATA as output port
+    clr LCD_RW ;  Only writing to the LCD in this code.
+	
+	mov a, #0ch ; Display on command
+	lcall LCD_command
+	mov a, #38H ; 8-bits interface, 2 lines, 5x7 characters
+	lcall LCD_command
+	mov a, #01H ; Clear screen (Warning, very slow command!)
+	lcall LCD_command
+    
+    ; Delay loop needed for 'clear screen' command above (1.6ms at least!)
+    mov R1, #40
+Clr_loop:
+	lcall Wait40us
+	djnz R1, Clr_loop
+	ret
