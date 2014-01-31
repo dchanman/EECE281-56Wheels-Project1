09f8bc240c714268ef1ae69edbeba3070e3899e1
ninadkny <nina.dacanay@gmail.com>
2014-01-30 19:20:19 -0800

    Outlining Functions
    Functions, Inputs, and Variables

--- Serial_Port.asm
+++ Serial_Port.asm
@@ -4,7 +4,17 @@ $NOLIST
 ; 
 ;	Nina Dacanay and Glyn Han
 ;
-;	Function: 1.)
+;	Function:  1.)Takes and Reads the temperature from 
+;				 the K-Type thermocouple connected to 
+;				 the MCP3004 ADC Converter.
+;				  
+;			   2.)Displays the temperature to a Python
+;				 Strip Chart through the Serial Port.
+;	
+;	Inputs:    1.)Temperature
+;	Outputs:   2.)Temperature Strip Chart
+;
+;	Variables: 1.)Accumulator      
 ;----------------------------------------------------
 CSEG
 
