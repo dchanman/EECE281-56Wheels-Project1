4fe210152a0ffaf460611a612d6dd2d8a10e9ece
kyujinpark <pizzajohnrock@hotmail.com>
2014-02-02 18:21:49 -0800

    for LCD display

---
+++ LCD_A(1).asm
@@ -0,0 +1,87 @@
+$MODDE2
+org 0
+   ljmp mycode
+   
+Wait40us:
+	mov R0, #149
+X1: 
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	djnz R0, X1 ; 9 machine cycles-> 9*30ns*149=40us
+    ret
+
+LCD_command:
+	mov	LCD_DATA, A
+	clr	LCD_RS
+	nop
+	nop
+	setb LCD_EN ; Enable pulse should be at least 230 ns
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	clr	LCD_EN
+	ljmp Wait40us
+
+LCD_put:
+	mov	LCD_DATA, A
+	setb LCD_RS
+	nop
+	nop
+	setb LCD_EN ; Enable pulse should be at least 230 ns
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	clr	LCD_EN
+	ljmp Wait40us
+	    
+mycode:
+    mov SP, #7FH
+    clr a
+    mov LEDRA, a
+    mov LEDRB, a
+    mov LEDRC, a
+    mov LEDG, a
+    
+    ; Turn LCD on, and wait a bit.
+    setb LCD_ON
+    clr LCD_EN  ; Default state of enable must be zero
+    lcall Wait40us
+    
+    mov LCD_MOD, #0xff ; Use LCD_DATA as output port
+    clr LCD_RW ;  Only writing to the LCD in this code.
+	
+	mov a, #0ch ; Display on command
+	lcall LCD_command
+	mov a, #38H ; 8-bits interface, 2 lines, 5x7 characters
+	lcall LCD_command
+	mov a, #01H ; Clear screen (Warning, very slow command!)
+	lcall LCD_command
+    
+    ; Delay loop needed for 'clear screen' command above (1.6ms at least!)
+    mov R1, #40
+Clr_loop:
+	lcall Wait40us
+	djnz R1, Clr_loop
+
+	; Move to first column of first row	
+	mov a, #80H
+	lcall LCD_command
+		
+	; Display letter A
+	mov a, #'A'
+	lcall LCD_put
+    
+forever:
+	sjmp forever
+	
+end
