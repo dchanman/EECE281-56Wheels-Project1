64f2592c3039202c8eaef8c1d1a6df86aa20aef2
dchanman <derekchan1994@gmail.com>
2014-02-03 15:12:10 -0800

    Reorganized Repository
    Added some more files from Connect to the Documents folder, so now we
    have quick access to most resources.

---
+++ Documents/stripchart_sinewave.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import sys, time, math
+
+xsize=100
+   
+def data_gen():
+    t = data_gen.t
+    while True:
+       t+=1
+       val=100.0*math.sin(t*2.0*3.1415/100.0)
+       yield t, val
+
+def run(data):
+    # update the data
+    t,y = data
+    if t>-1:
+        xdata.append(t)
+        ydata.append(y)
+        if t>xsize: # Scroll to the left.
+            ax.set_xlim(t-xsize, t)
+        line.set_data(xdata, ydata)
+
+    return line,
+
+def on_close_figure(event):
+    sys.exit(0)
+
+data_gen.t = -1
+fig = plt.figure()
+fig.canvas.mpl_connect('close_event', on_close_figure)
+ax = fig.add_subplot(111)
+line, = ax.plot([], [], lw=2)
+ax.set_ylim(-100, 100)
+ax.set_xlim(0, xsize)
+ax.grid()
+xdata, ydata = [], []
+
+# Important: Although blit=True makes graphing faster, we need blit=False to prevent
+# spurious lines to appear when resizing the stripchart.
+ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=100, repeat=False)
+plt.show()
