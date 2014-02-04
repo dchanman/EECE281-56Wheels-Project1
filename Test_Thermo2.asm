3c51b53592e7374a732b5137d6544a59451f0920
dchanman <derekchan1994@gmail.com>
2014-02-04 00:12:22 -0800

    Completed Thermocouple Input Software
    Finished the Thermocouple Input program, it is written completely new in
    thermo2.asm. Once we're okay with the changes, I'll combine the program
    back into the original Thermocouple_Input.asm file.
    
    Use the file Test_Thermo2.asm with the hardware to see if we are
    accurately relaying the temperature.
    
    I've hidden a lot of the helper subroutines in the program, so when we
    use it in the main file, we only need two subroutines: "Initialize" and
    "Read".
    
    "Initialize" will set up all the ports so that we can accept data
    "Read" will read from the ADC and automatically store the data to a
    2-byte Temperature variable in Celsius.
    
    Refer to the header file for more information

--- Test_Thermo2.asm
+++ Test_Thermo2.asm
@@ -16,6 +16,7 @@ DSEG at 30H
 x:	ds 2
 y:	ds 2
 bcd:	ds 3
+Temperature_Measured:	ds 2
 
 BSEG
 mf:	dbit 1
@@ -24,6 +25,17 @@ CSEG
 $include(Thermo2.asm)
 $include(math16.asm)
 
+
+;Delay half a second	
+WaitHalfSec:
+	mov R2, #90
+L3: mov R1, #250
+L2: mov R0, #250
+L1: djnz R0, L1
+	djnz R1, L2
+	djnz R2, L3
+	ret
+	
 ; Look-up table for 7-seg displays
 BCD_LUT:
     DB 0C0H, 0F9H, 0A4H, 0B0H, 099H        ; 0 TO 4
@@ -43,6 +55,31 @@ Display:
     anl a, #0fh
     movc A, @A+dptr
     mov HEX1, A
+    
+    ; Display Digit 2
+    mov A, bcd+1
+    anl a, #0fh
+    movc A, @A+dptr
+    mov HEX2, A
+	; Display Digit 3
+    mov A, bcd+1
+    swap a
+    anl a, #0fh
+    movc A, @A+dptr
+    mov HEX3, A
+    
+    ; Display Digit 4
+    mov A, bcd+2
+    anl a, #0fh
+    movc A, @A+dptr
+    mov HEX4, A
+	; Display Digit 5
+    mov A, bcd+2
+    swap a
+    anl a, #0fh
+    movc A, @A+dptr
+    mov HEX5, A
+    
     ret
     
 init:
@@ -54,9 +91,17 @@ init:
 	mov LEDG, A
 forever:
 	lcall Thermocouple_Input_Read_ADC
-	mov LEDRB, R7
-	mov LEDRA, R6
-	lcall Thermocouple_Input_Delay
+	mov LEDRA, R7
+	mov LEDG, R6	
+	mov x, Temperature_Measured
+	mov x+1, Temperature_Measured+1
+	
+	lcall hex2bcd	
+	lcall Display
+	
+	lcall waithalfsec
+	lcall waithalfsec
+	
 	sjmp forever
 	
 END
