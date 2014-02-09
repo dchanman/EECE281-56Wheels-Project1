c074aa2e42437657472378e2e35859fe60438606
dchanman <derekchan1994@gmail.com>
2014-02-09 15:33:22 -0800

    Added SSR Files
    Simple class using P1.0 to activate/deactivate a BJT for the SSR relay.
    
    Test program included

---
+++ SSR.asm
@@ -0,0 +1,27 @@
+$NOLIST
+;----------------------------------------------------
+;	SSR 
+; 
+;	Derek Chan
+;	
+;	Function: Controls the SSR Relay box
+;	Constants and Variables to be declared: n/a
+;	Functions:	
+;				SSR_Enable 	- 	turns on the SSR
+;				SSR_Disable	-	turns off the SSR
+;----------------------------------------------------
+CSEG
+
+SSR_Init:
+	orl P1MOD, #00000001B
+	ret
+	
+SSR_Enable:
+	setb P1.0
+	ret
+
+SSR_Disable:
+	clr	P1.0
+	ret
+	
+$LIST
