b8ba0cb415974ff38e7bc5a617fd6df644016b4a
jgmhua <jhua@live.ca>
2014-02-09 17:48:30 -0800

    update to UI
    Implemented initialization function

--- User_Interface.asm
+++ User_Interface.asm
@@ -40,12 +40,6 @@ $NOLIST
 
 ;----------------------------------------------------
 CSEG
-
-User_Interface_Init:
-	;lcall Settings_Initialization	
-ret
-	
-
 ;Function: Gets the correct parameters for over control from the user	
 UI_Set_Up_Parameters:
 ;Settings_Initializations:
@@ -61,8 +55,9 @@ UI_Set_Up_Parameters:
 Settings_Initialization_nonwelcome:
 	lcall Display_soak_temp_set
 	lcall Wait_for_Values
-	mov soak_temp+0, bcd+0
-	mov soak_temp+1, bcd+1
+	mov soak_temperature+0, bcd+0
+	mov soak_temperature+1, bcd+1
+	mov soak_temperature+2, bcd+2
 	mov bcd+2, #0
 	mov bcd+0, #0
 	mov bcd+1, #0
@@ -72,40 +67,50 @@ Settings_Initialization_nonwelcome:
 	lcall Wait_for_Values
 	mov soak_time+0, bcd+0
 	mov soak_time+1, bcd+1
+	mov soak_time+2, bcd+2
 	mov bcd+2, #0
 	mov bcd+0, #0
 	mov bcd+1, #0 
 
 	lcall Display_reflow_temp_set
 	lcall Wait_for_Values
-	mov reflow_temp+0, bcd+0
-	mov reflow_temp+1, bcd+1
+	mov reflow_temperature+0, bcd+0
+	mov reflow_temperature+1, bcd+1
+	mov reflow_temperature+2, bcd+2 
 	mov bcd+2, #0 
 	mov bcd+0, #0
 	mov bcd+1, #0
 
 	lcall Display_reflow_time_set
 	lcall Wait_for_Values
-	mov reflow_temp+0, bcd+0
-	mov reflow_temp+1, bcd+1
+	mov reflow_time+0, bcd+0
+	mov reflow_time+1, bcd+1
+	mov reflow_time+2, bcd+2
 	mov bcd+2, #0 
 	mov bcd+0, #0
 	mov bcd+1, #0
+	lcall Display
 
 	lcall Display_Confirmation_message 
 
 ret
-
-
 ;Function: Waits for the user to enter a value, and leaves the loop if Switch17 is ;pressed
+Wait_for_Confirmation:
+	jnb KEY.2, Settings_Initialization_nonwelcome
+	jnb KEY.1, Return_function
+	jmp Wait_for_confirmation
+
+
 Wait_for_Values:
-Wait_for_Values_loop: 
+;Wait_for_Values_loop: 
+	lcall Display
 	lcall ReadNumber
-	jnc Wait_for_Values_loop
+	jnb KEY.1, wait_key0
+	jnc Wait_for_Values
 	lcall Shift_Digits
 	lcall Display
-	jnb KEY.1, wait_key0
-	ljmp Wait_for_Values_loop
+
+	ljmp Wait_for_Values
 
 
 wait_key0:
@@ -115,10 +120,44 @@ wait_key0:
 Return_function:
 	ret
 
-
-Wait_for_Confirmation:
-	jnb KEY.1, Return_function
-	jnb KEY.2, Settings_Initialization_nonwelcome
-	jmp Wait_for_confirmation
-
-$LIST
+WaitHalfSec:
+	mov R2, #90
+N3: mov R1, #250
+N2: mov R0, #250
+N1: djnz R0, N1 ; 3 machine cycles-> 3*30ns*250=22.5us
+	djnz R1, N2 ; 22.5us*250=5.625ms
+	djnz R2, N3 ; 5.625ms*90=0.5s (approximately)
+	ret
+	
+Display_R57:
+	mov dptr, #myLUT
+	; Display Digit 0
+    mov A, bcd+0
+    anl a, #0fh
+    movc A, @A+dptr
+    mov R5, A
+	; Display Digit 1
+    mov A, bcd+0
+    swap a
+    anl a, #0fh
+    movc A, @A+dptr
+    mov R6, A
+	; Display Digit 2
+    mov A, bcd+1
+    anl a, #0fh
+    movc A, @A+dptr
+    mov R7, A
+	; Display Digit 3
+    ;mov A, bcd+1
+    ;swap a
+    ;anl a, #0fh
+    ;movc A, @A+dptr
+    ;mov HEX3, A
+	; Display Digit 4
+    ;mov A, bcd+2
+    ;anl a, #0fh
+    ;movc A, @A+dptr
+    ;mov HEX4, A
+    ret
+	
+end
