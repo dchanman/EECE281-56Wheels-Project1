c4d81727139bdf3ca86d0f43b304488f638eb500
dchanman <derekchan1994@gmail.com>
2014-02-10 16:24:07 -0800

    Added Buzzer component
    Test class, header file, main file.

---
+++ Buzzer.asm
@@ -0,0 +1,106 @@
+$NOLIST
+;----------------------------------------------------
+;	Buzzer
+; 
+;	Derek Chan
+;	
+;	Function: Interface for the Buzzer component
+;
+;	Subroutines:
+;		Buzzer_Init:	Initlizes P1MOD, TMOD, ***DOES NOT ENABLE INTERRUPTS***
+;		Buzzer_ISR:		Makes the buzzer beep
+;		Buzzer_Start_Beep:	Starts the buzzer
+;		Buzzer_Stop_Beep:	Stops the buzzer
+;
+;	Interrupt:
+;		org 000BH
+;			ljmp Buzzer_ISR
+;	Registers:
+;		P1.5 - Buzzer
+;	Constants:
+;		BUZZER_CLK EQU 33333333
+;		BUZZER_FREQ EQU 2000
+;		BUZZER_T0_RELOAD EQU 65536-(BUZZER_CLK/(12*2*BUZZER_FREQ))
+;	Variables:
+;		Buzzer_Beep_Count	:	ds 1
+;
+;		Buzzer_Beep_Active		:	dbit 1		
+;		Buzzer_Continuous_Tone	:	dbit 1
+;----------------------------------------------------
+CSEG
+
+Buzzer_Init:
+	orl P1MOD, #00100000B	; P1.5 is an output
+	orl TMOD, #00000001B 	; GATE=0, C/T*=0, M1=0, M0=1: 16-bit timer
+	clr TR0 				; Disable timer 0
+	clr TF0
+	mov TH0, #high(BUZZER_T0_RELOAD)
+	mov TL0, #low(BUZZER_T0_RELOAD)
+	setb ET0				; Enable T0 interrupt
+	
+	;;;;;;;;
+	;TODO: PRIORITIZE T0 OVER OTHER INTERRUPTS
+	;;;;;;;;
+	
+	setb Buzzer_Beep_Active
+	clr Buzzer_Continuous_Tone
+	
+	ret
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Buzzer_ISR
+;;
+;;Interrupt service routine for the buzzer. Causes
+;;the buzzer to beep according to set parameters.
+;;
+;;@param	-	Buzzer_Continuous_Tone:	
+;;					Buzzer goes BEEEEEEEEEEEP if set to 1,
+;;					Buzzer goes beep-beep-beep... if set to 0
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+Buzzer_ISR:
+	push acc
+	push psw
+	jb Buzzer_Continuous_Tone, Buzzer_ISR_Make_Beep
+	
+	clr C
+	mov A, Buzzer_Beep_Count
+	add A, #1
+	mov Buzzer_Beep_Count, A
+
+	jnc Buzzer_ISR_Check_Beep
+	cpl Buzzer_Beep_Active
+
+Buzzer_ISR_Check_Beep:
+	jnb Buzzer_Beep_Active, Buzzer_ISR_end	
+	
+Buzzer_ISR_Make_Beep:
+	clr A
+	mov Buzzer_Beep_Count, A
+	cpl P1.5
+	
+Buzzer_ISR_end:
+	mov TH0, #high(BUZZER_T0_RELOAD)
+	mov TL0, #low(BUZZER_T0_RELOAD)
+	pop psw
+	pop acc
+	reti
+		
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Buzzer_Start_Beep
+;;
+;;Starts the buzzer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+Buzzer_Start_Beep:
+	setb TR0
+	ret
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Buzzer_Stop_Beep
+;;
+;;Stops the buzzer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+Buzzer_Stop_Beep:
+	clr TR0
+	ret
+	
+$LIST
---
+++ Test_Buzzer.asm
@@ -0,0 +1,62 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;Test_Buzzer
+;;
+;;Test program for the buzzer module.
+;;
+;;SW0 - Turns buzzer on/off
+;;SW1 - Sets buzzer to beep or long-tone
+;;
+;;P1.5 is used for the buzzer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+$modde2
+
+BUZZER_CLK EQU 33333333
+BUZZER_FREQ EQU 2000
+BUZZER_T0_RELOAD EQU 65536-(BUZZER_CLK/(12*2*BUZZER_FREQ))
+
+org 0000H
+	ljmp init
+	
+org 000BH
+	ljmp Buzzer_ISR
+	
+DSEG at 30H
+Buzzer_Beep_Count	:	ds 1
+
+BSEG
+Buzzer_Beep_Active		:	dbit 1
+Buzzer_Continuous_Tone	:	dbit 1
+
+CSEG
+$include(Buzzer.asm)
+
+init:
+	clr A
+	mov LEDRA, A
+	mov LEDRB, A
+	mov LEDRC, A
+	mov LEDG, A
+	lcall Buzzer_Init
+	setb EA
+	
+forever:
+	mov A, SWA
+	jnb ACC.0, forever_stopbeep
+		mov LEDG, #10000000B
+		lcall Buzzer_Start_Beep
+		sjmp forever_checksw1
+forever_stopbeep:
+		mov LEDG, #00000000B
+		lcall Buzzer_Stop_Beep
+
+forever_checksw1:
+	jnb ACC.0, forever_longtone
+		mov LEDRA, #00000001B
+		clr Buzzer_Continuous_Tone
+		sjmp forever
+forever_longtone:
+		mov LEDRA, #00000000B
+		setb Buzzer_Continuous_Tone
+		sjmp forever
+		
+END
