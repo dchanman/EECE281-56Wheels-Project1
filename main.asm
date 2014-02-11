62427af04a49f23d1b106cfebd71eae9d008d087
dchanman <derekchan1994@gmail.com>
2014-02-10 18:41:23 -0800

    State Machine v1.0
    still in beta

--- main.asm
+++ main.asm
@@ -2,6 +2,9 @@ $modde2
 
 org 0000H
    ljmp main_init
+   
+org 000BH
+	ljmp Buzzer_ISR
       
 ;Thermocouple Constants
 FREQ	EQU 33333333
@@ -17,7 +20,9 @@ CE_EE	EQU  P0.4
 CE_RTC	EQU  P0.5 
 
 ;Buzzer Constants
-
+BUZZER_CLK EQU 33333333
+BUZZER_FREQ EQU 2000
+BUZZER_T0_RELOAD EQU 65536-(BUZZER_CLK/(12*2*BUZZER_FREQ))
 
 ;State Constants
 STATE_STANDBY 	EQU 0
@@ -40,6 +45,13 @@ reflow_time		 		: ds 2
 Temperature_Measured	: ds 2
 Outside_Temperature_Measured:	ds 2
 
+;Buzzer Variables
+Buzzer_Beep_Count	:	ds 1
+Buzzer_Beep_Num		:	ds 1
+
+;Timer
+elapsed_time			: ds 1
+
 ;Math16/32 Variables
 x						: ds 2
 y						: ds 2
@@ -48,17 +60,14 @@ op						: ds 1
 
 ;State
 state					: ds 1
-elapsed_time			: ds 1
 
 BSEG
-;LCD_Init Variables
-;;lcd_on			 		:  dbit 1
-;;LCD_EN			 		:  dbit 1
-;;LCD_MOD			 		:  dbit 1
-;;LCD_RW          		:  dbit 1
-
-;Thermocouple Registers (mf from math16/32)
+;Math16
 mf 						:  dbit 1
+
+;Buzzer bit variables
+Buzzer_Beep_Active		:	dbit 1		
+Buzzer_Continuous_Tone	:	dbit 1
 
 
 CSEG
@@ -66,11 +75,37 @@ CSEG
 $include(math16.asm)
 $include(SSR.asm)
 $include(Serial_Port.asm)
-;$include(Thermocouple_Input.asm)
+$include(Buzzer.asm)
 $include(Thermo2.asm)
 $include(User_Interface.asm)
 $include(LCD_Display.asm)
 ;$include(Read_sw5.asm)
+
+;;;;
+;;main_Maintain_Temperature(var temp)
+;;
+;;Maintains the desired temperature given in var temp
+;;;;
+main_Maintain_Temperature MAC
+	lcall Thermocouple_Update
+	mov x+0, Temperature_Measured+0
+	mov x+1, Temperature_Measured+1
+	mov y+0, %0+0
+	mov y+1, %0+1
+	
+	lcall x_lt_y
+	lcall main_Maintain_Temperature_helper
+	
+ENDMAC
+
+main_Maintain_Temperature_helper:
+	jb mf, main_Maintain_Temperature_tooCold
+	;too hot:
+	lcall SSR_Disable
+	ret
+main_Maintain_Temperature_tooCold:
+	lcall SSR_Enable
+	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;main_state_standby
@@ -101,7 +136,21 @@ main_state_standby:
 ;;	*Temperature_Measured == soak_temperature
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 main_state_heating1:
-	;lcall UI_Heating
+	lcall SSR_Enable	
+	
+	lcall Thermocouple_Update	
+	mov x+0, Thermocouple_Update+0
+	mov x+1, Thermocouple_Update+1
+	mov y+0, soak_temperature+0
+	mov y+1, soak_temperature+1
+	
+	lcall x_lt_y
+	jb mf, main_state_heating1_done	
+	mov state, #STATE_SOAK
+	Buzzer_Beep_Multiple(4)
+	;lcall resetElapsedTime
+	
+main_state_heating1_done:	
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -116,6 +165,21 @@ main_state_heating1:
 ;;		*elapsed_time == soak_time 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 main_state_soak:
+	main_Maintain_Temperature(soak_temperature)
+	
+	mov x+0, elapsed_time+0
+	mov x+1, elapsed_time+1
+	mov y+0, soak_time+0
+	mov y+1, soak_time+1
+	
+	lcall x_lt_y
+	jb mf, main_state_soak_done
+	
+	mov state, #STATE_HEATING2
+	Buzzer_Beep_Multiple(4)
+	;lcall resetElapsedTime
+
+main_state_soak_done:
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -129,6 +193,21 @@ main_state_soak:
 ;;		*Temperature_Measured == reflow_temperature
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 main_state_heating2:
+	lcall SSR_Enable	
+	
+	lcall Thermocouple_Update	
+	mov x+0, Thermocouple_Update+0
+	mov x+1, Thermocouple_Update+1
+	mov y+0, reflow_temperature+0
+	mov y+1, reflow_temperature+1
+	
+	lcall x_lt_y
+	jb mf, main_state_heating2_done	
+	mov state, #STATE_REFLOW
+	Buzzer_Beep_Multiple(4)
+	;lcall resetElapsedTime
+	
+main_state_heating2_done:	
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -143,34 +222,72 @@ main_state_heating2:
 ;;		*elapsed_time == reflow_time
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 main_state_reflow:
+	main_Maintain_Temperature(reflow_temperature)
+	
+	mov x+0, elapsed_time+0
+	mov x+1, elapsed_time+1
+	mov y+0, reflow_time+0
+	mov y+1, reflow_time+1
+	
+	lcall x_lt_y
+	jb mf, main_state_reflow_done
+	
+	mov state, #STATE_COOLDOWN
+	Buzzer_Beep_Multiple(4)
+	;lcall resetElapsedTime
+
+main_state_reflow_done:
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;main_state_cooldown
 ;;
 ;;Function:
-;;	*Waits for oven temperature to decrease to approx
-;;		0.5*reflow_temperature
+;;	*Beep until the door opens
 ;;
 ;;State Change:
 ;;	STATE_OPEN_DOOR:
-;;		*temperature_measured == 0.5*reflow_temperature
+;;		*Door_Open == true
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 main_state_cooldown:
+	lcall Thermocouple_Update
+	lcall SSR_Disable
+	lcall Buzzer_Start_Beep
+	
+	;lcall Door_Check
+	;jnb Door_Open, main_state_cooldown_done
+	mov state, #STATE_OPEN_DOOR
+	lcall Buzzer_Stop_Beep
+	;lcall elapsed_time
+	
+main_state_cooldown_done:	
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;main_state_open_door
 ;;
 ;;Function:
-;;	*Makes three beep sounds
+;;	*Waits until component is cool
 ;;
 ;;State Change:
 ;;	STATE_STANDBY:
-;;		*door opens (if feature available)
-;;		*Or after the beeps are finished
+;;		*Temperature_Measured < 40 degrees C
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 main_state_open_door:
+	lcall Thermocouple_Update	
+	mov x+0, Thermocouple_Update+0
+	mov x+1, Thermocouple_Update+1
+	mov y+0, low(40)
+	mov y+1, high(40)
+	
+	lcall x_gt_y
+	jb mf, main_state_open_door_done	
+	mov state, #STATE_STANDBY
+	Buzzer_Beep_Multiple(3)
+	;lcall resetElapsedTime
+	
+main_state_open_door_done:	
+	
 	ret
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
